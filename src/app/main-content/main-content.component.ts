7c3cba27107f22fdf945843f604feaab4939ff4d
Hummner <bence.cservenyak@gmail.com>
2025-06-08 20:09:40 +0200

    Contact Form added and work in progress

--- src/app/main-content/main-content.component.ts
+++ src/app/main-content/main-content.component.ts
@@ -4,11 +4,12 @@ import { AboutMeComponent } from "./about-me/about-me.component";
 import { SkillsComponent } from "./skills/skills.component";
 import { PortfolioComponent } from "./portfolio/portfolio.component";
 import { TopCommentsComponent } from "./top-comments/top-comments.component";
+import { ContactComponent } from "./contact/contact.component";
 
 @Component({
   selector: 'app-main-content',
   standalone: true,
-  imports: [HeroComponent, AboutMeComponent, SkillsComponent, PortfolioComponent, TopCommentsComponent],
+  imports: [HeroComponent, AboutMeComponent, SkillsComponent, PortfolioComponent, TopCommentsComponent, ContactComponent],
   templateUrl: './main-content.component.html',
   styleUrl: './main-content.component.scss'
 })

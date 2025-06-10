aa85f3295609f4d031dab23d593e7d0c69381565
Hummner <bence.cservenyak@gmail.com>
2025-06-10 08:03:31 +0200

    privacy-policy Page added without Style

---
+++ src/app/privacy-policy/privacy-policy.component.ts
@@ -0,0 +1,38 @@
+import { CommonModule } from '@angular/common';
+import { Component } from '@angular/core';
+import { TranslateModule, TranslateService } from '@ngx-translate/core';
+
+@Component({
+  selector: 'app-privacy-policy',
+  standalone: true,
+  imports: [TranslateModule, CommonModule],
+  templateUrl: './privacy-policy.component.html',
+  styleUrl: './privacy-policy.component.scss'
+})
+export class PrivacyPolicyComponent {
+
+  sectionKeys = [
+    'general',
+    'controller',
+    'dataCollection',
+    'logfiles',
+    'cookies',
+    'rights',
+    'form',
+    'ssl',
+    'changes'
+  ];
+
+  constructor(private translate: TranslateService) {
+    translate.addLangs(['de', 'en']);
+    translate.setDefaultLang('en');
+    translate.use('en');
+  }
+
+  switchLanguage() {
+    const newLang = this.translate.currentLang === 'de' ? 'en' : 'de';
+    this.translate.use(newLang);
+  }
+
+
+}

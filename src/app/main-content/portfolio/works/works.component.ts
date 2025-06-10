e1b85de95d4ae55fbd920bb4b51540a3e4c25a5c
Hummner <bence.cservenyak@gmail.com>
2025-06-10 21:16:30 +0200

    bugfixed, legal notice and imprint Style added

--- src/app/main-content/portfolio/works/works.component.ts
+++ src/app/main-content/portfolio/works/works.component.ts
@@ -12,6 +12,8 @@ import { TranslateModule, TranslateService } from '@ngx-translate/core';
 export class WorksComponent {
   images = ["laptop_0"];
   projectKeys = ['join', 'sharkie', 'crm', 'pokedex'];
+  projectLinks = ['http://join.bence-cservenyak.com', 'http://sharkie.bence-cservenyak.com', '', 'http://pokodex.bence-cservenyak.com/']
+  githubLinks = ['https://github.com/Hummner/join-448', 'https://github.com/Hummner/sharkie', '', 'https://github.com/Hummner/Pokodex']
 
   constructor(private translate: TranslateService) {
     translate.addLangs(['de', 'en']);
@@ -23,6 +25,9 @@ export class WorksComponent {
     const newLang = this.translate.currentLang === 'de' ? 'en' : 'de';
     this.translate.use(newLang);
   }
+
+
+
 
 
 

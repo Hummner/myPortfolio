10b5b9404969486b1610ecf901d05c550a1d18b3
Hummner <bence.cservenyak@gmail.com>
2025-11-17 08:01:14 +0100

    add: dububble aplha version

--- src/app/main-content/portfolio/works/works.component.ts
+++ src/app/main-content/portfolio/works/works.component.ts
@@ -13,7 +13,7 @@ import { AfterViewChecked } from '@angular/core';
 })
 export class WorksComponent implements AfterViewInit, AfterViewChecked {
   projectKeys = ['join', 'sharkie', 'kanMind', 'dabubble'];
-  projectLinks = ['http://join.bence-cservenyak.com', 'http://sharkie.bence-cservenyak.com', 'noLink','inProg']
+  projectLinks = ['http://join.bence-cservenyak.com', 'http://sharkie.bence-cservenyak.com', 'noLink','https://dabubble.bence-cservenyak.com/']
   githubLinks = ['https://github.com/Hummner/join-448', 'https://github.com/Hummner/sharkie', 'https://github.com/Hummner/KanMind', 'https://github.com/Hummner/DABubble']
   private aosInitialized = false;
 
